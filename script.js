a89ed05a2c34952094cd030c0a11d6976b1a3857
TUNGBULU DOUYE <54280188+Tun-Dev@users.noreply.github.com>
2020-05-03 17:00:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function BMI(){
+    var h = document.getElementById('Height').value;
+    var w = document.getElementById('Weight').value;
+    var bm = w/(h/100*h/100);
+    var bmi = (bm.toFixed(2));
+
+    if (h === "" || w === ""){
+        window.alert("Please type in your Height and Weight");
+        return
+    }
+
+    document.getElementById("Result").innerHTML= "Your BMI is " + bmi;
+
+    
+}
