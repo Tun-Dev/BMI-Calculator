40e7e310c6deaec3babf735a84e27ab8af01f59b
TUNGBULU DOUYE <54280188+Tun-Dev@users.noreply.github.com>
2020-05-05 11:46:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,8 @@ function BMI(){
     var bm = w/(h/100*h/100);
     var bmi = (bm.toFixed(2));
 
-    if (h === "" || w === ""){
+    if (h == 0 || h === "" || w === ""){
         window.alert("Please type in your Height and Weight");
-        return
-    }
-    else if (h <= 0){
-        window.alert ("Please type in your Height");
         return
     }
 
