9797dff36d0b92a250a895a7507f36e32616c627
TUNGBULU DOUYE <54280188+Tun-Dev@users.noreply.github.com>
2020-05-08 12:13:55 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,12 +4,18 @@ function BMI(){
     var bm = w/(h/100*h/100);
     var bmi = (bm.toFixed(2));
 
-    if (h == 0 || h === "" || w === ""){
+    if (h === "" || w === ""){
         window.alert("Please type in your Height and Weight");
-        return
+        return;
     }
 
-    document.getElementById("Result").innerHTML= "Your BMI is " + bmi;
+    document.getElementById("Result").innerHTML= "Your BMI is " + bmi + "<br><h5>BMI Categories:</h5><h6>Underweight = <18.5</h6><h6>Normal weight = 18.5 - 24.9</h6><h6>Overweight = 25 - 29.9</h6><h6>Obesity = BMI of 30 or higher</h6>";
 
     
 }
+
+//BMI Categories:
+//Underweight = <18.5
+//Normal weight = 18.5–24.9
+//Overweight = 25–29.9
+//Obesity = BMI of 30 or greater
